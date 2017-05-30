d7b03f88c85660f3c3d9af6a6e2d97657f13f18b
Nguyen Nguyen <nguyen.nguyen@devop5.io>
2017-05-30 17:44:29 +0700

    add room and model

--- app/src/main/java/nhn/calendarapp/MainActivity.kt
+++ app/src/main/java/nhn/calendarapp/MainActivity.kt
@@ -1,12 +1,18 @@
 package nhn.calendarapp
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import nhn.calendarapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
+
+
 }
---
+++ app/src/main/java/nhn/calendarapp/model/task.kt
@@ -0,0 +1,7 @@
+package nhn.calendarapp.model
+
+/**
+ * Created by nguyennguyen on 30/5/17.
+ */
+
+data class Task (var id: String, var taskName: String, var taskSummary: String, var date: String, var time:String)
